fec25a5771897a64e730b080ec56fca7f12b45a6
Shivtej Gaikwad <91597404+ShivtejG236@users.noreply.github.com>
2025-11-03 13:34:00 +0530

    Update auth.js

--- Frontend/auth.js
+++ Frontend/auth.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   const googleBtn = document.getElementById("google-login");
   googleBtn.addEventListener("click", () => {
     // Redirect user to backend Google OAuth endpoint
-    window.location.href = "https://turing-web-version.up.railway.app/auth/google";
+    window.location.href = "https://turing-web-forked.onrender.com/auth/google";
   });
 });
