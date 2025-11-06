c486a4ffa412d046e9e25e7e6abbc1ac5d111358
Shivtej Gaikwad <91597404+ShivtejG236@users.noreply.github.com>
2025-11-06 23:44:31 +0530

    Update auth.js

--- Frontend/auth.js
+++ Frontend/auth.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
     window.location.href = "https://turing-web-version.onrender.com/auth/google";
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const googleBtn = document.getElementById("github-login");
+  githubBtn.addEventListener("click", () => {
+    // Redirect user to backend Google OAuth endpoint
+    window.location.href = "https://turing-web-version.onrender.com/auth/github";
+  });
+});
