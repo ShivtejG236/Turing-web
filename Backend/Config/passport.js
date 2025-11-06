ccfef3f4e2e8a4ab9592b93f71bf818d403f5f2a
Shivtej Gaikwad <91597404+ShivtejG236@users.noreply.github.com>
2025-11-06 23:47:56 +0530

    Update passport.js

--- Backend/Config/passport.js
+++ Backend/Config/passport.js
@@ -1,11 +1,23 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import { Strategy as GitHubStrategy } from "passport-github2";
 import { config } from "./env.js";
 
+// Google Strategy
 passport.use(new GoogleStrategy({
   clientID: config.google.clientID,
   clientSecret: config.google.clientSecret,
   callbackURL: config.google.callbackURL
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}));
+
+// GitHub Strategy
+passport.use(new GitHubStrategy({
+  clientID: config.github.clientID,
+  clientSecret: config.github.clientSecret,
+  callbackURL: config.github.callbackURL,
+  scope: ['user:email']
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
